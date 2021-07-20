0cf84ffe61e9baa0d01c50acef088d4c03bc042b
THIBRUYS <thibruys@gmail.com>
2021-07-20 14:43:14 +0200

    added form validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -16,16 +16,34 @@ const AddUser = () => {
 
     const addUserHandler = (event) =>{
         event.preventDefault();
+        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+            return
+        };
+        if (+enteredAge < 1) {
+            return
+        };
         console.log(enteredUserName, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
     return (
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text' onChange={userNameChangeHandler} value={enteredUserName}/>
+                <input 
+                    id='username' 
+                    type='text' 
+                    onChange={userNameChangeHandler} 
+                    value={enteredUserName}
+                />
                 <label htmlFor='age'>Age (years)</label>
-                <input id='age' type='number' onChange={ageChangeHandler} value={enteredAge}/>
+                <input 
+                    id='age' 
+                    type='number' 
+                    onChange={ageChangeHandler} 
+                    value={enteredAge}
+                />
                 <Button buttonType='submit'> add user</Button>
             </form>
         </Card>
