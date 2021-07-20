f818f52da979add8c92c519ca564947a21747c8b
THIBRUYS <thibruys@gmail.com>
2021-07-20 13:29:13 +0200

    Added a Card component
    Created and implemented a reusable Card component. Added a CSS file to AddUser as well and pass its styles to the Card component via props

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from '../UI/Card'
+import styles from './AddUser.module.css'
+
 const AddUser = () => {
     const addUserHandler = (event) =>{
         event.preventDefault();
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text' />
-            <label htmlFor='age'>Age (years)</label>
-            <input id='age' type='number' />
-            <button type='submit'> add user</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input id='username' type='text' />
+                <label htmlFor='age'>Age (years)</label>
+                <input id='age' type='number' />
+                <button type='submit'> add user</button>
+            </form>
+        </Card>
     );
 }
 
